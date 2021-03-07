3364aac6f53ce77783b3b09df12d2506ec3bdccf
Patrick Duvall <duvallp208@gmail.com>
2021-03-07 13:57:48 -0700

    Add index template with base html nav bar

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    alerts = [
+        {
+            'email': 'jon@test.com',
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'email': 'pat@test.com',
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, alerts=alerts)
